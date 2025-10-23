9190d8135e07feaa6020fc33563eefba1c355327
CraigDavao <davaojonathancraig28@gmail.com>
2025-10-23 23:56:46 +0800

    first deployment, update to online db

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 define("SITE_URL", "http://localhost/JDSystem/");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "jolly_database");
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "jolly_database");
 
-// define("DB_HOST", "b3sl32ekxzydrb5wscjh-mysql.services.clever-cloud.com");
-// define("DB_USER", "ujowc00747fduqvq");
-// define("DB_PASS", "mDScdVXVx0pc74ibViLe");
-// define("DB_NAME", "b3sl32ekxzydrb5wscjh");
+define("DB_HOST", "b3sl32ekxzydrb5wscjh-mysql.services.clever-cloud.com");
+define("DB_USER", "ujowc00747fduqvq");
+define("DB_PASS", "mDScdVXVx0pc74ibViLe");
+define("DB_NAME", "b3sl32ekxzydrb5wscjh");
 ?>
