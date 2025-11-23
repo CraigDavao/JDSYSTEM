86712238db656143a1495b960c78e4ad6a24d721
CraigDavao <davaojonathancraig28@gmail.com>
2025-11-23 20:15:39 +0800

    a

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 require_once __DIR__ . '/includes/header.php';
+require_once __DIR__ . '/connection/connection.php';
 
 /* ------------------------------------------------------------
    HELPER FUNCTION: Get default color ID for a product
