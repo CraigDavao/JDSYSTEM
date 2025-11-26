dfbc18191ec77e2da291d9cca5de89c0c752561f
CraigDavao <davaojonathancraig28@gmail.com>
2025-11-26 10:11:05 +0800

    a

--- config.php
+++ config.php
@@ -5,11 +5,7 @@
 // define("DB_PASS", "");
 // define("DB_NAME", "jolly_database");
 
-// config.php - FIXED FOR HOSTINGER
-// define("SITE_URL", "http://localhost/JDSystem/");
-
-// For Hostinger - use absolute URL
-define("SITE_URL", "https://jollydolly.shop/JDSystem/");
+define("SITE_URL", getenv('SITE_URL') ?: '/');
 
 define("DB_HOST", "srv1319.hstgr.io");
 define("DB_USER", "u251504662_group6");
